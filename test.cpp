1647095b6287ecddb88b95712d5dcc0d2339019a
강경록 <91719160+liliililliiil@users.noreply.github.com>
2022-09-27 23:37:16 +0900

    practice

---
+++ test.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+namespace A
+{
+    int num = 100;
+}
+namespace B
+{
+    int num = 200;
+}
+
+int main()
+{
+    int num = 1000;
+    const int size = 20;
+    const char* c = "안녕하세요";
+
+    std::cout << A::num << '\n';// endl 끝? endline?
+    std::cout << B::num << '\n';
+    std::cout << "main num : " << num << "\n\n";
+    std::cout << "인사할게요 : " << c << "\n\n";
+
+    return 0;
+}
