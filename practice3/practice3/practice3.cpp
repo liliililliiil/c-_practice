aba687025ca96edfd24df3c3304346e22a748f9a
강경록 <91719160+liliililliiil@users.noreply.github.com>
2022-09-27 23:04:48 +0900

    pr

---
+++ practice3/practice3/practice3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+double Circle_area(int r)
+{
+    return 3.14 * r * r;
+}
+
+int main()
+{
+    int n = 8;
+    std::cout << "n + 9 = " << n + 9 << "\n";
+    std::cout << "원의 넓이는 = " << Circle_area(n);
+}
